e31322025ff23c1e44723b7e76f34ed2fafe0f92
Marcin Mirecki <markky21@gmail.com>
2023-06-16 16:08:22 +0200

    Initial commit

---
+++ jest.config.ts
@@ -0,0 +1,15 @@
+/* eslint-disable */
+export default {
+  displayName: 'nextjs-13',
+  preset: './jest.preset.js',
+  transform: {
+    '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nx/react/plugins/jest',
+    '^.+\\.[tj]sx?$': ['babel-jest', { presets: ['@nx/next/babel'] }],
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  coverageDirectory: './coverage/nextjs-13',
+  testMatch: [
+    '<rootDir>/src/**/__tests__/**/*.[jt]s?(x)',
+    '<rootDir>/src/**/*(*.)@(spec|test).[jt]s?(x)',
+  ],
+};
