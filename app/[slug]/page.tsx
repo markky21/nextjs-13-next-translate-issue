60c7c42d266a4106c40c163b409a8d90f9983d8d
markky21 <markky21@gmail.com>
2023-06-16 16:34:48 +0200

    chore: add test case

---
+++ app/[slug]/page.tsx
@@ -0,0 +1,19 @@
+interface Props {
+  params: { slug: string };
+}
+
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
+export const dynamicParams = true;
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
+export const revalidate = 'force-cache';
+
+export default function Page({ params: { slug } }: Props) {
+  return (
+    <>
+      <h1>Hello, Home page! 2</h1>
+      <div>slug: {slug}</div>
+    </>
+  );
+}
---
+++ app/slug/page.tsx
@@ -0,0 +1,7 @@
+export default function Page() {
+  return (
+    <>
+      <h1>Hello, Home page! SLUG</h1>
+    </>
+  );
+}
