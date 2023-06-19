c79280b9e9193450ff9c5dfa4588a08c7ec27684
markky21 <markky21@gmail.com>
2023-06-19 08:06:16 +0200

    chore: add test case

--- next.config.js
+++ next.config.js
@@ -1,9 +1,9 @@
 //@ts-check
 
 // eslint-disable-next-line @typescript-eslint/no-var-requires
-const { composePlugins, withNx } = require('@nx/next');
-const nextTranslate = require('next-translate-plugin')
-const CopyPlugin = require('copy-webpack-plugin')
+const {  withNx } = require('@nx/next');
+const nextTranslate = require('next-translate-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
 
 /**
  * @type {import('@nx/next/plugins/with-nx').WithNxOptions}
@@ -18,27 +18,21 @@ const nextConfig = {
     // Note: we provide webpack above so you should not `require` it
     // Perform customizations to webpack config
     config.plugins.push(
-        new CopyPlugin({
-          patterns: [
-            {
-              from: './i18n.js',
-              to: '../i18n.js',
-            },
-          ],
-        })
-    )
+      new CopyPlugin({
+        patterns: [
+          {
+            from: './i18n.js',
+            to: '../i18n.js',
+          },
+        ],
+      })
+    );
 
     // Important: return the modified config
-    return config
+    return config;
   },
 };
 
-const plugins = [
-  // Add more Next.js plugins to this list if needed.
-  withNx,
-  // nextTranslate
-];
-
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-ignore
-module.exports = composePlugins(...plugins)(nextConfig);
+module.exports = withNx(nextTranslate(nextConfig));
